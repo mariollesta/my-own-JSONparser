9f96d70f2bb1c570f0a9e51e3ad347e36aa16884
mariollesta <llesta.mario@gmail.com>
2023-12-09 16:29:54 +0100

    🚧 work in progress...

--- myjsonp/myjsonp/main.py
+++ myjsonp/myjsonp/main.py
@@ -24,45 +24,51 @@ app = typer.Typer(rich_markup_mode="rich")
 
 # Functions
 
+def read_file(file:str):
+    content = None
+    
+    try:
+        if file:
+            with open(file, 'r') as f:
+                content = f.read()
+        else:
+            content = sys.stdin.buffer.read()
+        
+        return content
+    
+    except FileNotFoundError:
+        print(f"Error: File not found.")
+           
+    except Exception as e:
+        print(f"Error: {e}")
+        
+
+def json_parser(file:str):
+    
+    content = read_file(file)
+    
+    try:
+        json_data = json.loads(content)
+        
+        if isinstance(json_data, dict):
+            return 0
+        
+    
+    except json.JSONDecodeError as e:
+        return 1 
+
+
 @app.command(epilog="Made by [green]mariollesta[/green]") 
 def myjsonp(
     file:str = typer.Argument("", help="File PATH"), 
 ):
     
-    try:
-        
-        # 0 -> valid JSON, 1 -> invalid JSON
-        valid = 0
-        invalid = 1    
-        
-        content = None
-        
-        if file:
-            with open(file, 'r') as f:
-                content = f.read()
-        
-        else:
-            content = sys.stdin.buffer.read()
-    
-        json_data = json.loads(content)
-        print(json_data)
-        
-        if isinstance(json_data, dict):
-            print(valid)
-        else:
-            print(invalid)
-         
-              
-    except json.JSONDecodeError as e:
-                  print(f"Error decoding JSON.")          
-
-    except FileNotFoundError:
-        print(f"Error: File not found.")
-           
-    except Exception as e:
-        print(f"Error: {e}")     
+    # 0 -> valid JSON
+    # 1 -> invalid JSON 
+    result = "0. Valid JSON" if json_parser(file) == 0 else "1. Invalid JSON"
+    print(result)   
     
 
-
+     
 if __name__ == "__main__":
     app()  
---
+++ myjsonp/tests/test_myjsonparser.py
@@ -0,0 +1,92 @@
+import unittest
+import logging
+
+from unittest.mock import patch
+from assertpy import assert_that
+
+from myjsonp.main import json_parser
+
+
+
+class TestMyJSONParser(unittest.TestCase):
+    
+    def test_json_parser_valid_json_step1(self):
+        # Validating an empty JSON...
+        test_file_path = 'tests/step1/valid.json'
+        result = json_parser(test_file_path)
+        assert_that(result, 0)
+        
+    
+    def test_json_parser_valid_json_step2(self):
+        # Validating a simple JSON object containing string keys and string values...
+        test_file_path = 'tests/step2/valid.json'
+        result = json_parser(test_file_path)
+        assert_that(result, 0)
+    
+    
+    def test_json_parser_valid_json_step2_2(self):
+        # Validating a simple JSON object containing string keys and string values...
+        test_file_path = 'tests/step2/valid2.json'
+        result = json_parser(test_file_path)
+        assert_that(result, 0)
+    
+        
+    def test_json_parser_valid_json_step3(self):
+        # Validating a simple JSON object containing string, numeric, boolean and null values...
+        test_file_path = 'tests/step3/valid.json'
+        result = json_parser(test_file_path)
+        assert_that(result, 0)
+     
+        
+    def test_json_parser_valid_json_step4(self):
+        # Validating a simple JSON object with object and array values...
+        test_file_path = 'tests/step4/valid.json'
+        result = json_parser(test_file_path)
+        assert_that(result, 0)
+    
+    
+    def test_json_parser_valid_json_step4_2(self):
+        # Validating a simple JSON object with object and array values...
+        test_file_path = 'tests/step4/valid2.json'
+        result = json_parser(test_file_path)
+        assert_that(result, 0)
+        
+        
+    def test_json_parser_invalid_json_step1(self):
+        # Validating an empty JSON...
+        test_file_path = 'tests/step1/invalid.json'
+        result = json_parser(test_file_path)
+        assert_that(result, 1)
+    
+    
+    def test_json_parser_invalid_json_step2(self):
+        # Validating a simple JSON object containing string keys and string values...
+        test_file_path = 'tests/step2/invalid.json'
+        result = json_parser(test_file_path)
+        assert_that(result, 1)
+    
+        
+    def test_json_parser_invalid_json_step2_2(self):
+        # Validating a simple JSON object containing string keys and string values...
+        test_file_path = 'tests/step2/invalid2.json'
+        result = json_parser(test_file_path)
+        assert_that(result, 1)
+    
+    
+    def test_json_parser_invalid_json_step3(self):
+        # Validating an invalid simple JSON object containing string, numeric, boolean and null values...
+        test_file_path = 'tests/step3/invalid.json'
+        result = json_parser(test_file_path)
+        assert_that(result, 1)
+        
+    
+    def test_json_parser_invalid_json_step4(self):
+        # Validating an invalid simple JSON object with object and array values...
+        test_file_path = 'tests/step4/invalid.json'
+        result = json_parser(test_file_path)
+        assert_that(result, 1)
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
